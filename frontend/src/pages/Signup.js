33418efa6afb12df1b8a0cb61ce801efcd84ea7f
md-hani <hani13may@yahoo.com>
2024-01-25 09:25:34 -0600

    Added splitPane  to Signup page

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,8 +1,20 @@
 import Header from '../components/Header'
+import SplitPane from 'react-split-pane'
 
 const Signup = () => {
     return (
-        <Header />
+        <div>
+            <Header />
+            <SplitPane split='vertical' minSize='68%' defaultSize='68%' maxSize='68%'>
+                <div className='topPane'>
+                    <div className='Welcometoamc'>Welcome To AMC</div>
+                    <input className='inputBoxSignUp' type='text'/>
+                </div>
+                <div className='bottomPane'>
+
+                </div>
+            </SplitPane>
+        </div>
     )
 }
 
