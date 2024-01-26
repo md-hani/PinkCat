983f9c5f203cc1439666295ff57389a463769168
md-hani <hani13may@yahoo.com>
2024-01-26 14:13:59 -0600

    Completed SignIn UI

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,19 +1,30 @@
 import Header from '../components/Header'
-import SplitPane from 'react-split-pane'
+import { Link } from "react-router-dom"
 
 const Signin = () => {
     return (
-        <div>
-            <Header />
-            <SplitPane split='vertical' minSize='68%' defaultSize='68%' maxSize='68%'>
-                <div className='leftPaneSignIn'>
-                    <div className='WelcomeBack'>Welcome Back</div>
-                        <input className='inputBox' type='text'/>
+        <div className='mainDivSI'>
+            <Header className='headerSU' />
+            <div className='paneDivSI'>
+                <div className='leftPaneSI'>
+                    <div className='Welcomeback'>Welcome Back</div>
+                    <form>
+                        <div className='inputAreaSI'>
+                            <label className='inputBoxLabelSI'>Name<input size={25} className='inputBoxSI' type='text' placeholder='John Doe' name='nameInput' required/></label>                      
+                        </div>
+                        <div className='inputAreaSI'>
+                            <label className='inputBoxLabelSI'>Password<input size={25} className='inputBoxSI' type='password' placeholder='*****' name='nameInput' required/></label>                      
+                        </div>
+                        <div className='inputAreaSI'>
+                            <label className='inputBoxLabelSI'><input size={25} className='inputButtonSI' type='submit' value='LOG IN' name='submitSignup'/></label>                      
+                        </div>
+                    </form>
                 </div>
-                <div className='rightPaneSignIn'>
-
+                <div className='rightPaneSI'>
+                    <div className='newHere'>NEW<br/>HERE?</div>
+                    <Link to='/signup'><button className='signinNavSI' type='button'>SIGN UP</button></Link>
                 </div>
-            </SplitPane>
+            </div>
         </div>
     )
 }
