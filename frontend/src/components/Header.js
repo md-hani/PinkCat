194d402ae3bc9cb7bb40f5eb21465f238e075a6b
md-hani <hani13may@yahoo.com>
2024-02-03 12:12:10 -0600

    fixed extended clicking area for logo problem.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,9 @@
 import logo from '../assets/AMC Logo Transparent.png'
-import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
         <nav>
-            <div className="logo">
-                <Link to='/'><img src={logo} alt="logo" /></Link>
-            </div>
+            <a className='myLinkNav'href='/'><img className='logo' src={logo} alt="logo" /></a>
         </nav>
     )
 }
