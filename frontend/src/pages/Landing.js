a03d81c50ba75b9f77ee7b46158310b4bf07ae6a
md-hani <hani13may@yahoo.com>
2024-02-07 12:07:53 -0600

    Fixed json.parse error

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -2,17 +2,11 @@ import React from 'react'
 import { Link } from "react-router-dom"
 
 const Landing = () => {
-    const [data, setData] = React.useState('');
+    //const [data, setData] = React.useState('');
 
-    React.useEffect(() => {
-        fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
-    }, []);
 
     return (
         <div className="landing">
-            <h2>{data}</h2>
             <h2><Link to='/signup'>Signup</Link></h2>
             <h2><Link to='/signin'>SignIn</Link></h2>
         </div>
